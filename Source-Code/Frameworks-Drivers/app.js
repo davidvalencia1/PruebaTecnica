5897c8d59c7139128527791ba4708009e74cfa5e
davidvalencia1 <114039494+davidvalencia1@users.noreply.github.com>
2024-11-04 16:11:55 -0500

    Build: Inicializacion y estructuracion del proyecto
    Se realiza la inicalizacion del proyecto y se le da una estructura base de carpetas. junto con la instalacion de las principales dependencias que se van a usar durante este

---
+++ Source-Code/Frameworks-Drivers/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
