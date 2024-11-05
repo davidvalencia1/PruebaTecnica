561f680a2320d8a2508171510646cee3fd73cb57
davidvalencia1 <114039494+davidvalencia1@users.noreply.github.com>
2024-11-04 22:08:42 -0500

    Style: Se da formado a archivos y adiccion de comentarios faltantes
    Se da formato usando Prettier a archivos sin formatear
    Se añaden comentarios al archivo de Producto-Controller

--- Source-Code/Interface-Adapters/Controllers/Product-Controller.js
+++ Source-Code/Interface-Adapters/Controllers/Product-Controller.js
@@ -2,35 +2,48 @@ const ProductUseCases = require("../../Use-Cases/Product-Use-Cases");
 
 class ProductController {
      constructor() {
+          // Inicializacion de Casos de Uso
           this.productUseCases = new ProductUseCases();
      }
 
+     // Metodo para Creacion de Producto
      async createProduct(req, res) {
+          // Prepracion de datos para creacion de Producto
           try {
+               // Extraer datos del cuerpo de la solicitud (request body)
                const { name, description, price, category } = req.body;
+               // LLamado al caso de uso para crear un producto
                const product = await this.productUseCases.createProduct(
                     name,
                     description,
                     price,
                     category
                );
+               // Se envia respuesta exitosa con el producto creado
                res.status(201).json(product);
           } catch (error) {
+               // Se envia respuesta con mensaje de error en caso de fallo
                res.status(500).json({ error: error.message });
           }
      }
 
+     // Metodo de listado para todos los Productos
      async listProducts(req, res) {
           try {
+               // LLamado de caso de uso para listar Productos
                const arrayProducts = await this.productUseCases.listProduct();
+               // Retorna la lista de los productos pbtenidos junto con codigo de estado de la request
                res.status(200).json(arrayProducts);
           } catch (error) {
+               // Se retorna error en caso de fallo
                res.status(500).json({ error: error.message });
           }
      }
 
+     // Metodo de obtener un producto por ID
      async getByIdProduct(req, res) {
           try {
+               // LLamado de caso de caso de uso para obtener un producto por ID
                const product = await this.productUseCases.getProduct(
                     req.params.id
                );
@@ -40,10 +53,14 @@ class ProductController {
           }
      }
 
+     // Metodo para actualizar un Producto
      async updateProduct(req, res) {
           try {
+               // Se obtiene ID de los parametros de la solicitud
                const { id } = req.params;
+               // Se exrae del cuerpo de la solicitud los datos de producto a actualizar
                const { name, description, price, category } = req.body;
+               // LLamado de caso de uso para actualizar el producto con sus respectivos parametros
                const product = await this.productUseCases.updateProduct(
                     id,
                     name,
@@ -57,6 +74,7 @@ class ProductController {
           }
      }
 
+     // Metodo para eliminar un producto
      async deleteProduct(req, res) {
           try {
                const response = await this.productUseCases.deleteProduct(
--- Source-Code/Use-Cases/Product-Use-Cases.js
+++ Source-Code/Use-Cases/Product-Use-Cases.js
@@ -1,6 +1,5 @@
-const Product = require('../Entities/Product');
-const ProductRepository = require('../Frameworks-Drivers/In-Memory-Repository/Product-Repository');
-
+const Product = require("../Entities/Product");
+const ProductRepository = require("../Frameworks-Drivers/In-Memory-Repository/Product-Repository");
 
 class ProductUseCases {
      constructor() {
@@ -8,13 +7,17 @@ class ProductUseCases {
           this.productRepository = new ProductRepository();
      }
 
-     
      async createProduct(name, description, price, category) {
           // Crea un producto asignadole la fecha de creacion antes de hacerlo
           const createdAt = new Date().toISOString();
-          const product = new Product(name, description, price, category, createdAt);
+          const product = new Product(
+               name,
+               description,
+               price,
+               category,
+               createdAt
+          );
           return await this.productRepository.save(product);
-
      }
 
      async listProduct() {
@@ -25,27 +28,28 @@ class ProductUseCases {
      async getProduct(id) {
           // Busca un producto por ID, en caso de no ser encontrado retorna error
           const product = await this.productRepository.findById(id);
-          if (!product) throw new Error('Product not found');
+          if (!product) throw new Error("Product not found");
           return await product;
      }
 
      async updateProduct(id, name, description, price, category) {
           // Busca el producto existente por ID
           const existingProduct = await this.productRepository.findById(id);
-          if (!existingProduct) throw new Error('Product not found');
-      
+          if (!existingProduct) throw new Error("Product not found");
+
           // Crea un nuevo objeto Product, si el parametro pasado no esta definido usa los parametros
           // del producto ya existe
           const updatedProduct = new Product(
-              name !== undefined ? name : existingProduct.name,
-              description !== undefined ? description : existingProduct.description,
-              price !== undefined ? price : existingProduct.price,
-              category !== undefined ? category : existingProduct.category
+               name !== undefined ? name : existingProduct.name,
+               description !== undefined
+                    ? description
+                    : existingProduct.description,
+               price !== undefined ? price : existingProduct.price,
+               category !== undefined ? category : existingProduct.category
           );
-          updatedProduct.id = existingProduct.id,  // Mantiene el ID existente
-      
-          // Guarda el producto actualizado en el repositorio y despues lo devuelve
-          await this.productRepository.save(updatedProduct);
+          (updatedProduct.id = existingProduct.id), // Mantiene el ID existente
+               // Guarda el producto actualizado en el repositorio y despues lo devuelve
+               await this.productRepository.save(updatedProduct);
 
           return await this.productRepository.findById(updatedProduct.id);
      }
@@ -53,15 +57,10 @@ class ProductUseCases {
      async deleteProduct(id) {
           // Busca un producto y lo elimina, en caso de que no exista retorna error
           const product = await this.productRepository.findById(id);
-          if (!product) throw new Error('Product not found');
+          if (!product) throw new Error("Product not found");
           this.productRepository.delete(id);
-          return 'delete product correctly';
+          return "delete product correctly";
      }
-
-
-
 }
-
-
 
 module.exports = ProductUseCases;
