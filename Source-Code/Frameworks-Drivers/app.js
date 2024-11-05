3273e7c97379c9cd37e54151f35f18a5861c1a9c
davidvalencia1 <114039494+davidvalencia1@users.noreply.github.com>
2024-11-04 23:03:37 -0500

    Feat: Captura de errores generado en pruebas
    Se Implementa captura cuando haya errores en el formato de los JSON de las request
    Se implementa captura de errores generado al solicitar una ruta inexistente
    Se implementa captura de errores cuando no se brinda un parametro

--- Source-Code/Frameworks-Drivers/app.js
+++ Source-Code/Frameworks-Drivers/app.js
@@ -5,8 +5,27 @@ const productRoutes = require("../Interface-Adapters/Routes/Product-Routes");
 const app = express();
 app.use(bodyParser.json());
 
-//establecer la ruta base para products
+// Establecer la ruta base para products
 app.use("/products", productRoutes);
+
+// Captura errores de Syntax en las requests
+app.use((err, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).send({ error: 'JSON inválido' });
+    }
+    next();
+});
+
+// Atrapa errores generados al solicitar una ruta no existente
+app.use((res) => {
+    res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
+// Atrapa errores cuando no se brinda un parametro a una ruta que lo requiere
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({error: 'an error occurred'});
+});
 
 //Iniciar servidor en el puerto 3000
 const PORT = process.env.PORT || 3000;
